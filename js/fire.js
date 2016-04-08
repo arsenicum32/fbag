86a10019502648888cced7b601ddbfe5ac3ef7c4
arsenicum32 <zombiehot@yandex.ru>
2016-04-09 00:02:18 +0300

    deploy

---
+++ js/fire.js
@@ -0,0 +1,7 @@
+$('fire').on('click', function(){
+  if($('#phone').val() && $('#name').val()){
+    $.get('http://myapisite.tk/neworder?m='+$('#name').val()+' телефон: '+$('#phone').val()+' заказ сумки с bagfruit.ga', function(){
+
+    });
+  }
+})
